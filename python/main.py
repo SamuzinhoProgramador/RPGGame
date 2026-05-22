eb0682b33ba7dd791dd99573c8be64394b7e793d
SamuzinhoProgramador <samuelbomfimbordin@gmail.com>
2026-05-22 16:57:19 -0300

    pythonic!

--- python/main.py
+++ python/main.py
@@ -17,80 +17,87 @@ class Main:
     def create_character(self):
         inserted_name = input("Enter the name of your character:\n")
 
-        self.valid_answer = False 
-        while not self.valid_answer:
-            print("Choose your class: 1 - Warrior, 2 - Wizard, 3 - Ranger, 4 - Rogue")
-            class_type_input = input()
-            
-            # Validação para garantir que o usuário digitou um número
-            if not class_type_input.isdigit():
-                print("Please enter a valid number.")
-                continue
-            
-            class_type = int(class_type_input)
+        while True:
+            try:
+                class_type = int(input("Choose your class: 1 - Warrior, 2 - Wizard, 3 - Ranger, 4 - Rogue"))
 
-            if class_type == 1:
-                self.player = Character(inserted_name, "Warrior", 150, 0, 0)
-                self.player.inventory.extend(["sword", "health potion"])
-                self.player.attacks.append(Attack(0, 1,"sword", 8, False, False))
-                self.player.attacks.append(Attack(0, 1, "punch", 4, False, False))
-                self.valid_answer = True
-            elif class_type == 2:
-                self.player = Character(inserted_name, "Wizard", 100, 0, 100)
-                self.player.inventory.append("staff")
-                self.player.attacks.append(Attack(30, 1, "fire ball", 12, True, False))
-                self.valid_answer = True
-            elif class_type == 3:
-                self.player = Character(inserted_name, "Ranger", 130, 8, 0)
-                self.player.inventory.append("bow")
-                self.player.attacks.append(Attack(0, 1, "arrow", 6, False, True))
-                self.valid_answer = True
-            elif class_type == 4:
-                self.player = Character(inserted_name, "Rogue", 110, 12, 0)
-                self.player.inventory.append("dagger")
-                self.player.attacks.append(Attack(0, 1, "dagger", 4, False, False))
-                self.valid_answer = True
+            except ValueError:
+                print("Please enter a number.\n")
+                input()                 
+
             else:
-                print("Invalid class type. Please type 1, 2, 3, or 4.")
 
-        print("\nCharacter created successfully!")
-        print(self.player) 
-        print(f"Inventory: {self.player.inventory}")
-        
-        attacks_str = ", ".join([str(attack) for attack in self.player.attacks])
-        print(f"Character attacks: [{attacks_str}]")
+                if class_type < 1 or class_type > 4:
+                    print("The value must be 1, 2, 3 or 4\n")
+                    input()
+                    continue #se não for válido, reinicia o while
 
-        self.start_adventure() 
+                if class_type == 1:
+                    self.player = Character(inserted_name, "Warrior", 150, 0, 0)
+                    self.player.inventory.extend(["sword", "health potion"])
+                    self.player.attacks.append(Attack(0, 1,"sword", 8, False, False))
+                    self.player.attacks.append(Attack(0, 1, "punch", 4, False, False))
+                elif class_type == 2:
+                    self.player = Character(inserted_name, "Wizard", 100, 0, 100)
+                    self.player.inventory.append("staff")
+                    self.player.attacks.append(Attack(30, 1, "fire ball", 12, True, False))
+                elif class_type == 3:
+                    self.player = Character(inserted_name, "Ranger", 130, 8, 0)
+                    self.player.inventory.append("bow")
+                    self.player.attacks.append(Attack(0, 1, "arrow", 6, False, True))
+                elif class_type == 4:
+                    self.player = Character(inserted_name, "Rogue", 110, 12, 0)
+                    self.player.inventory.append("dagger")
+                    self.player.attacks.append(Attack(0, 1, "dagger", 4, False, False))
+
+                print("\nCharacter created successfully!")
+                input()
+                print(self.player) 
+                print(f"Inventory: {self.player.inventory}")
+                attacks_str = ", ".join([str(attack) for attack in self.player.attacks])
+                print(f"Character attacks: [{attacks_str}]")
+                input()
+                self.start_adventure()
+                break
 
     def start_adventure(self):
         friendship = 7
         print("\nYou are in a tavern, in a vast world called Manta.")
+        input()
         print("The bartender approaches you and says:") 
+        correctAnswer = False
         
-        self.valid_answer = False 
-        while not self.valid_answer:         
-            print("\n'I've never seen you around here in Stix, welcome. Would you like something?'")
-            print("1 - Thanks! I'll have a vodka!")
-            print("2 - Ignore")
-            print("3 - Go f*** yourself!")
+        while correctAnswer == False:
+            try:
+                print("\n'I've never seen you around here in Stix, welcome. Would you like something?'")
+                input()
+                print("1 - Thanks! I'll have a vodka!")
+                print("2 - Ignore")
+                print("3 - Go f*** yourself!")
+                
+                response = int(input())
             
-            response = input()
+            except ValueError:
+                print("Please enter a number.\n")
+                input()
 
-            if response == "1":
-                friendship += 1
-                print("He brings you a vodka.")
-                self.valid_answer = True
-            elif response == "2":
-                friendship -= 1
-                print("You ignore him. He shrugs and goes to serve another table.")
-                self.valid_answer = True
-            elif response == "3":
-                friendship -= 3
-                print("The bartender frowns and mutters something.")
-                self.valid_answer = True
             else:
-                print("Invalid option. Choose 1, 2, or 3.")
-
+                if response < 1 or response > 3:
+                    print("Please enter a number that correspondes with one of the options")
+                    continue #reinicia o while
+                else:
+                    correctAnswer = True
+                    
+                if response == "1":
+                    friendship += 1
+                    print("He brings you a vodka.")
+                elif response == "2":
+                    friendship -= 1
+                    print("You ignore him. He shrugs and goes to serve another table.")
+                elif response == "3":
+                    friendship -= 3
+                    print("The bartender frowns and mutters something.")
+                
 if __name__ == "__main__":
     game = Main()
     game.start_game()
